7fa863aba2295bb1779f6a77429610b79b8d6d37
henanleo <139044215+henanleo@users.noreply.github.com>
2025-12-08 14:07:55 +0800

    Add files via upload

---
+++ GMR/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'general_motion_retargeting',
+  packages = find_packages(),
+  author="Yanjie Ze",
+  author_email="lastyanjieze@gmail.com",
+  description="General Motion Retargeting (GMR) for Humanoid Robots",
+  long_description=open("README.md").read(),
+  long_description_content_type="text/markdown",
+  url="https://github.com/YanjieZe/GMR",
+  license="MIT",
+  version="0.2.0",
+  python_requires='>=3.10',
+)
